bbfdadb6e5c669ba0f6aaf08ddaf9e32da2f7779
MaanavN <maanav0114@gmail.com>
2025-03-14 13:57:28 -0400

    Created random loading and different personalities

--- app.py
+++ app.py
@@ -3,8 +3,13 @@ from dotenv import load_dotenv #type: ignore
 import os
 from openai import OpenAI #type: ignore
 import time
+import pandas as pd
+import random
+
+
 
 load_dotenv()
+
 
 
 app = Flask(__name__)
@@ -15,8 +20,16 @@ client = OpenAI(
     api_key=os.getenv("API_KEY"),
 )
 
-@app.route('/')
-def home():
+
+
+uncs = pd.DataFrame(pd.read_csv('uncs.csv'))
+def create_cards():
+    idxs = []
+    while len(idxs) < len(uncs):
+        idxs.append(random.randint(0, len(uncs)-1))
+        idxs = list(set(idxs))
+    random.shuffle(idxs)
+
     cards = [
         {
             'heading': 'UncR',
@@ -24,33 +37,60 @@ def home():
             'name': '',
             'image': 'https://upload.wikimedia.org/wikipedia/commons/5/59/Empty.png',
             'description': ''
-        },
-        {
-            'heading': '',
-            'subheading': '',
-            'name': 'Suresh',
-            'image': 'https://img.freepik.com/free-photo/indian-man-portrait-temple_53876-14535.jpg',
-            'description': 'Loves feet'
-        },
-        {
-            'heading': '',
-            'subheading': '',
-            'name': 'Niggesh',
-            'image': 'https://i0.wp.com/journals-times.com/wp-content/uploads/2023/09/Rajessh-M-Iyer-photo.png',
-            'description': 'likes little boys'
         }
     ]
+    for idx in idxs:
+        unc = {
+            'heading': '',
+            'subheading': '',
+            'name': str(uncs.iloc[idx][0]),
+            'image': str(uncs.iloc[idx][2]),
+            'description': str(uncs.iloc[idx][1])
+        }
+        cards.append(unc)
+    
+    return cards
+
+
+
+@app.route('/')
+def home():
+    # cards = [
+    #     {
+    #         'heading': 'UncR',
+    #         'subheading': 'Find your perfect unc!<br>Swipe ← for No &nbsp; Swipe → for Yes',
+    #         'name': '',
+    #         'image': 'https://upload.wikimedia.org/wikipedia/commons/5/59/Empty.png',
+    #         'description': ''
+    #     },
+    #     {
+    #         'heading': '',
+    #         'subheading': '',
+    #         'name': 'Suresh',
+    #         'image': 'https://img.freepik.com/free-photo/indian-man-portrait-temple_53876-14535.jpg',
+    #         'description': 'Loves feet'
+    #     },
+    #     {
+    #         'heading': '',
+    #         'subheading': '',
+    #         'name': 'Niggesh',
+    #         'image': 'https://i0.wp.com/journals-times.com/wp-content/uploads/2023/09/Rajessh-M-Iyer-photo.png',
+    #         'description': 'likes little boys'
+    #     }
+    # ]
+    cards = create_cards()
 
     return render_template('index.html', cards=cards)
 
 
 
-def message_ai(message):
+def message_ai(message, person):
+    time.sleep(3)
     response = client.chat.completions.create(
         model="cognitivecomputations/dolphin3.0-mistral-24b:free",
         messages=[{
             "role": "system",
-            "content": "Respond in a flirtatious way like you want to pursue a potential relationship. Utilize genZ slang."
+            "content": f"Respond in a flirtatious way like you want to pursue a potential relationship. Utilize genZ slang. Your name is {person['name']}. Your description is {person['description']}"
         }, {
             "role": "user",
             "content": message
@@ -65,10 +105,11 @@ def message_ai(message):
 @app.route('/chat', methods=['POST'])
 def chat():
     user_message = request.json.get('message')
+    person = request.json.get('person')
 
     while True:
         try:
-            response = message_ai(user_message)
+            response = message_ai(user_message, person)
             break
         except Exception as e:
             print(f"ran into {e} retrying")
